febd1bccf98bac972ccceeadec0dc5db38f0927a
klozowski <kennylozowski@gmail.com>
2017-08-01 17:42:44 -0600

    a bunch of updates with playback inluding internal queueing system

--- index.php
+++ index.php
@@ -13,9 +13,22 @@
 <script src="js/jquery-3.2.1.min.js"></script>
 <script src="js/polarmusic.js"></script>
 <script>
-$(function() {
-    WebMidiStart();
-});
+    
+    CONSOLE_DEBUGGING = true;
+
+    $(function() {
+        var northPole = new PolarSynth({
+            synthString : "VirtualMIDISynth #1",
+            synthDefaultChannel : 1,
+            centreReference : new Point(152,224),
+            minNote : 24,
+            maxNote : 96,
+            scaleLength: 12,
+            scaleNotes: [0,2,3,5,7,8,7,10]
+        });
+        northPole.WebMidiStart();
+
+    });
 
 </script>
 </head>
