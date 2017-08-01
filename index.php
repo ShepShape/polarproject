affd31f8b663dbc396c508117ac6c72e91439a92
klozowski <kennylozowski@gmail.com>
2017-08-01 17:47:19 -0600

    a bunch of changes

--- index.php
+++ index.php
@@ -13,9 +13,8 @@
 <script src="js/jquery-3.2.1.min.js"></script>
 <script src="js/polarmusic.js"></script>
 <script>
-    
-    CONSOLE_DEBUGGING = true;
 
+    CONSOLE_DEBUGGING = true;
     $(function() {
         var northPole = new PolarSynth({
             synthString : "VirtualMIDISynth #1",
@@ -23,16 +22,25 @@
             centreReference : new Point(152,224),
             minNote : 24,
             maxNote : 96,
-            scaleLength: 12,
-            scaleNotes: [0,2,3,5,7,8,7,10]
+            scaleLength : 12,
+            scaleNotes : [1,2,4,5,7,9,11],
+            lengthInSeconds : 1800,
+            tempoBPM : 70,
+            tempoLengths : [0.25,0.5,1,2,4]
         });
         northPole.WebMidiStart();
 
+
+        $("#stop_all").click(function() {
+            northPole.stopAllNotes();
+        });
     });
+
+
 
 </script>
 </head>
 <body>
-
+<button id="stop_all">Stop All Notes!</button>
 </body>
 </html>
--- js/polarmusic.js
+++ js/polarmusic.js
@@ -1,92 +1,93 @@
-//configuration settings, set these as you wish
-
-CONSOLE_DEBUGGING = true;
-SYNTH1_SUBSTRING = "VirtualMIDISynth #1";
-SYNTH1_CHANNEL1 = 1;
-NORTH_CENTRE_REFERENCE = new Point(152,224);
-NORTH_MIN_NOTE = 24;
-NORTH_MAX_NOTE = 96;
-NORTH_SCALE_LENGTH = 12;
-NORTH_SCALE_ARR = [0,2,3,5,7,8,7,10]
 
 
-//program variables
-var midiOut1;
-var timeCounter = 0;
-var northPoints = new Array();
-var northMinRadius=-1;
-var northMaxRadius=-1;
+var PolarSynth = (function() {
 
-function WebMidiStart() {
-    WebMidi.enable(configSynths); //this enables the WebMIDI library and calls the configSynths function when this is done
-}
-
-function configSynths(err) {
-    debug(WebMidi.outputs); //spits out all the available WebMIDI outputs to the console
-    midiOut1 = WebMidi.getOutputByName(SYNTH1_SUBSTRING); //sets a midi output object by using the midi output string name as a reference, find the string name in the output from the above line
-    $.getJSON( "icefiles/northice.json", parseGeoJSON); //get the json file in the first argument and call the parseGeoJSON function after getting it
-    //startMusic();
-}
+    var midiOut,params;
+    var timeCounter = 0;
+    var northPoints = new Array();
+    var northMinRadius=-1;
+    var northMaxRadius=-1;
+    var temp_Current_Time = 0;
+    var noteQueue = new Array();
 
 
-function parseGeoJSON(data) {
-    var coords; //an array of all the points in a feature
-    var p; //individual point object from the json file
-    var pPoint; // the new polarPoint object we will declare
-    for (var i=0;i<data.features.length;i++) { // iterate through the features (closed polygons)
-        coords = data.features[i].geometry.coordinates[0]; //assign a shorthand for all the coordinates in a feature (called coords)
-        for(var j=0;j<coords.length;j++) { //iterate through the coordinates in the feature
-            p = coords[j];
-            pPoint = new PolarPoint(p[0],p[1],NORTH_CENTRE_REFERENCE); //define the current polarpoint object from the point in the geoJSON file
-            if ((northMinRadius == -1) || (pPoint.r < northMinRadius)) northMinRadius = pPoint.r; //set the minimum radius fro the current pPoint if it has the smallest value to date;
-            if ((northMaxRadius == -1) || (pPoint.r > northMaxRadius)) northMaxRadius = pPoint.r; //set the minimum radius fro the current pPoint if it has the smallest value to date;
-            northPoints.push(pPoint); //add the pPoint object to the array
+    function PolarSynth(p) {
+        params = p;
+    }
+
+    PolarSynth.prototype.WebMidiStart = function() {
+        WebMidi.enable(configSynths); //this enables the WebMIDI library and calls the configSynths function when this is done
+    }
+
+    PolarSynth.prototype.stopAllNotes = function() {
+        while(noteQueue.length>0) {
+            clearTimeout(noteQueue.pop());
         }
     }
-    northPoints.sort(angleSort); //sort the Array of points by angle
 
-    startMusic(); //now we play some music!
-}
-
-function translateRadiusToPitch(val,min,max,pMin,pMax,pScaleLength,pScaleArr) {
-    var in_mapping = val / (max - min); //translate the radius to an abstract number between 0 and 1;
-    var num_notes = Math.floor((pMax-pMin)/pScaleLength*pScaleArr.length); // this gives you the number of notes available to you considering the number of octaves and the number of notes in a scale in a single octave
-    var raw_output = Math.floor(num_notes*in_mapping); //maps the radius to a note in the available notes;
-    var num_octaves = Math.floor((pMax-pMin)/pScaleLength);
-    var current_octave = Math.floor(raw_output / pScaleArr.length);
-    var scale_note = raw_output % pScaleArr.length;
-    //console.log(num_octaves+" "+current_octave+" "+scale_note);
-    var output = pMin+(pScaleLength*current_octave)+pScaleArr[scale_note];
-    return output;
-}
-
-function translateAngleToTime(val) {
-    var mapping = val / 360;
-    var output = Math.floor(mapping * 1800000);
-    return output
-}
-
-function startMusic() {
-    var notePoint,notePitch,noteTime;
-    for(var i=0;i<northPoints.length;i++) {
-        notePoint = northPoints[i];
-        notePitch = translateRadiusToPitch(notePoint.r,northMinRadius,northMaxRadius,NORTH_MIN_NOTE,NORTH_MAX_NOTE,NORTH_SCALE_LENGTH,NORTH_SCALE_ARR);
-        noteTime = translateAngleToTime(notePoint.t);
-        console.log(i+" "+notePitch+" "+noteTime);
-        midiOut1.playNote(notePitch,SYNTH1_CHANNEL1,{time:noteTime,duration:150});
+    var configSynths = function(err) {
+        debug(WebMidi.outputs); //spits out all the available WebMIDI outputs to the console
+        midiOut= WebMidi.getOutputByName(params.synthString); //sets a midi output object by using the midi output string name as a reference, find the string name in the output from the above line
+        $.getJSON( "icefiles/northice.json", parseGeoJSON); //get the json file in the first argument and call the parseGeoJSON function after getting it
     }
 
-   /** var randNote, randLength;
-    var numNotes = Math.floor(Math.random()*100)+10;
-    for (i=0;i<numNotes;i++) {
-        randNote = Math.floor(Math.random()*127);
-        randLength = Math.floor(Math.random()*400)+100;
-        midiOut1.playNote(randNote,SYNTH1_CHANNEL1,{duration:randLength,time:timeCounter});
-        timeCounter+=randLength;
-    } */
 
 
-}
+
+    var parseGeoJSON = function(data) {
+        var coords; //an array of all the points in a feature
+        var p; //individual point object from the json file
+        var pPoint; // the new polarPoint object we will declare
+        for (var i=0;i<data.features.length;i++) { // iterate through the features (closed polygons)
+            coords = data.features[i].geometry.coordinates[0]; //assign a shorthand for all the coordinates in a feature (called coords)
+            for(var j=0;j<coords.length;j++) { //iterate through the coordinates in the feature
+                p = coords[j];
+                pPoint = new PolarPoint(p[0],p[1],params.centreReference); //define the current polarpoint object from the point in the geoJSON file
+                if ((northMinRadius == -1) || (pPoint.r < northMinRadius)) northMinRadius = pPoint.r; //set the minimum radius from the current pPoint if it has the smallest value to date;
+                if ((northMaxRadius == -1) || (pPoint.r > northMaxRadius)) northMaxRadius = pPoint.r; //set the minimum radius from the current pPoint if it has the smallest value to date;
+                northPoints.push(pPoint); //add the pPoint object to the array
+            }
+        }
+        northPoints.sort(angleSort); //sort the Array of points by angle
+
+        startMusic(); //now we play some music!
+    }
+
+    var translateRadiusToPitch = function(val,min,max,pMin,pMax,scaleLength,pScaleArr) {
+        var input_mapping = val / (max - min); //translate the radius to an abstract number between 0 and 1;
+        var num_notes = Math.floor((pMax-pMin)/scaleLength*pScaleArr.length); // this gives you the number of notes available to you considering the number of octaves and the number of notes in a scale in a single octave
+        var raw_output = Math.floor(num_notes*input_mapping); //maps the radius to a note in the available notes;
+        var num_octaves = Math.floor((pMax-pMin)/scaleLength);
+        var current_octave = Math.floor(raw_output / pScaleArr.length);
+        var scale_note = raw_output % pScaleArr.length;
+        var output = pMin+(params.scaleLength*current_octave)+pScaleArr[scale_note];
+        return output;
+    }
+
+    var translateAngleToTime =  function(val) {
+        var mapping = val / 360;
+        var rawOutputTime = Math.floor(mapping * 1000 * params.lengthInSeconds);
+        var minQuantizeMillis = (60000 / params.tempoBPM) * params.tempoLengths[0];
+        var quantizedOutputTime = Math.round(Math.round(rawOutputTime/minQuantizeMillis)*minQuantizeMillis);
+        return quantizedOutputTime;
+    }
+
+    var startMusic = function () {
+        var notePoint,notePitch,noteTime;
+        for(var i=0;i<northPoints.length;i++) {
+            notePoint = northPoints[i];
+            notePitch = translateRadiusToPitch(notePoint.r,northMinRadius,northMaxRadius,params.minNote,params.maxNote,params.scaleLength,params.scaleNotes);
+            noteTime = translateAngleToTime(notePoint.t);
+            noteQueue.push(setTimeout(function(nP){
+                midiOut.playNote(nP,params.synthDefaultChannel,{duration:(Math.floor(Math.random()*150+150)),velocity:(Math.random()*0.6+0.2)});
+            },noteTime,notePitch));
+        }
+
+    }
+
+    return PolarSynth;
+
+}());
 
 // utility functions
 
