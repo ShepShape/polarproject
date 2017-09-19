1ffb16c51510c12879cd75d83eed8c192ff60dc8
Kenny Lozowski <kennylozowski@gmail.com>
2017-09-18 21:04:26 -0600

    finished internal MIDI integration

--- js/polarmusic.js
+++ js/polarmusic.js
@@ -1,24 +1,35 @@
 CONSOLE_DEBUGGING = true;
 var northPolePaths,southPolePaths;
+var internalMidiReady = false;
+var externalMidiReady = false;
+var synthsCreated = false;
 
-function createSynths(err) {
-    var northPole = new PolarSynth({
-        fileString : "icefiles/2017/1/2017-1-1_north.json",
-        internalOrExternal: "external",
-        externalSynthChannel : 1,
-        externalSynthString: "VirtualMIDISynth #1",
-        internalSynthGMPatch: 1,
-        internalSynthInstrument: "acoustic_grand_piano",
-        moveSpeed : 0.2
-    });
-   /** var southPole = new PolarSynth({
+function createSynths() {
+    if (internalMidiReady && externalMidiReady && !synthsCreated) {
+        synthsCreated = true;
+        var northPole = new PolarSynth({
+            fileString : "icefiles/2017/1/2017-1-1_north.json",
+            internalOrExternal: "internal",
+            externalSynthChannel : 1,
+            externalSynthString: "VirtualMIDISynth #1",
+            internalSynthGMPatch: 0,
+            internalSynthInstrument: "acoustic_grand_piano",
+            moveSpeed : 0.2
+        });
+        /** var southPole = new PolarSynth({
         fileString : "icefiles/2017/8/south_2017-8-30.json",
         synthDefaultChannel : 2,
     }); */
-    $("#stop_all").click(function() {
-        northPole.stopAllNotes();
-        southPole.stopAllNotes();
-    });
+        $("#stop_all").click(function() {
+            northPole.stopAllNotes();
+            southPole.stopAllNotes();
+        });
+    } else {
+        setTimeout(createSynths,500);
+    }
+
+
+
 }
 
 // utility functions
@@ -39,13 +50,13 @@ function drawInit() {
 
 function PolarSynth(p) {
     this.params = p;
-    this.externalMidiOut = (this.params.externalSynthString) ? WebMidi.getOutputByName(this.params.externalSynthString) : WebMidi.outputs[0] ;
-    this.externalMidiChannel = (this.params.externalSynthChannel) ? this.params.externalSynthChannel : 1;
+    this.externalMidiOut = (this.params.hasOwnProperty("externalSynthString")) ? WebMidi.getOutputByName(this.params.externalSynthString) : WebMidi.outputs[0] ;
+    this.externalMidiChannel = (this.params.hasOwnProperty("externalSynthChannel")) ? this.params.externalSynthChannel : 1;
+    this.internalMidiChannel = (this.params.hasOwnProperty("internalSynthGMPatch")) ?this.params.internalSynthGMPatch : 0;
     this.noteQueue = new Array();
     this.paths = new paper.Group();
     this.upSpeed = -1;
     if (this.params.moveSpeed) this.upSpeed = 0-this.params.moveSpeed;
-    console.log(this.upSpeed);
     this.lastNP = 0;
     this.lastVert = 0;
     this.vertOffset = 0;
@@ -59,7 +70,7 @@ function PolarSynth(p) {
     }
 
     this.resetSynth = function() {
-        console.log('the end');
+        debug('the end');
     }
 
     this.drawNote = function(nP,nT,nV) {
@@ -81,17 +92,20 @@ function PolarSynth(p) {
 
     this.startMusic = function (data) {
         var notePitch,noteTime,noteDuration,noteVelocity;
+        if (self.params.internalOrExternal == "external") {
+            MIDI.setVolume(self.internalMidiChannel, 127);
+        }
         for(var i=0;i<data.orderedPoints.length;i++) {
             notePitch = data.orderedPoints[i].pitch;
             noteTime = data.orderedPoints[i].time;
             noteDuration = data.orderedPoints[i].duration;
             noteVelocity = data.orderedPoints[i].velocity;
             self.noteQueue.push(setTimeout(function(nP,nT,nV){
-            self.noteQueue.push(setTimeout(function(nP,nT,nV){
                 if (self.params.internalOrExternal == "external") {
                     self.externalMidiOut.playNote(nP,self.externalMidiChannel,{duration:noteDuration,velocity:noteVelocity});
                 } else {
-
+                    MIDI.noteOn(self.internalMidiChannel, nP, noteVelocity, 0);
+                    MIDI.noteOff(self.internalMidiChannel, nP,(noteDuration/1000));
                 }
                 self.drawNote(nP,nT,nV);
             },noteTime,notePitch,noteTime,noteVelocity));
@@ -111,7 +125,24 @@ function PolarSynth(p) {
 $(function() {
     var canvas = $("#polarCanvas")[0];
     paper.setup(canvas);
-    WebMidi.enable(createSynths);
+    WebMidi.enable(function() {
+        debug('external MIDI subsystem loaded');
+        externalMidiReady = true;
+        createSynths();
+
+    });
+    MIDI.loadPlugin({
+        soundfontUrl: "./soundfont/",
+        instrument: "acoustic_grand_piano",
+        onprogress: function(state, progress) {
+            debug(state+" "+progress);
+        },
+        onsuccess: function() {
+            debug('internal MIDI subsystem loaded');
+            internalMidiReady = true;
+            createSynths();
+        }
+    });
 });
 
 
