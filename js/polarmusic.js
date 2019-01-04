db7f85f3f98769993f5feecea3bb8d21d730f58a
CMH\klozowski <kennylozowski@gmail.com>
2019-01-03 17:04:33 -0700

    put static SVG files in place

--- js/polarmusic.js
+++ js/polarmusic.js
@@ -5,7 +5,8 @@ LOAD_EXTERNAL_MIDI = false;
 var internalMidiReady = false;
 var externalMidiReady = false;
 var synthsCreated = false;
-var canvasWidth = 800;
+var canvasWidth = 1000;
+var pngWidth = 304;
 var globalUpSpeed = -0.2
 
 function createSynths() {
@@ -79,6 +80,7 @@ function PolarSynth(p) {
     this.internalMidiChannel = (this.params.hasOwnProperty("internalSynthGMPatch")) ?this.params.internalSynthGMPatch : 0;
     this.noteQueue = new Array();
     this.linePaths = new paper.Group();
+    this.mapPaths = new paper.Group();
     this.lineUpSpeed = -1;
     this.horzMultiplier = this.params.whichSide == "left" ? 1.0 : -1.0;
     this.sideBase = this.params.whichSide == "left" ? 0 : canvasWidth;
@@ -96,7 +98,7 @@ function PolarSynth(p) {
     }
 
     this.startSynth = function(whichDate,northOrSouth) {
-        var fileString = "icefiles/"+whichDate.getFullYear()+"/"+(whichDate.getMonth()+1)+"/"+whichDate.getFullYear()+"-"+(whichDate.getMonth()+1)+"-"+whichDate.getDate()+"_"+northOrSouth+".json";
+        var fileString = "icefiles/"+whichDate.getFullYear()+"/"+(whichDate.getMonth()+1)+"/"+whichDate.getFullYear()+"-"+(whichDate.getMonth()+1)+"-"+whichDate.getDate()+"_"+northOrSouth;
         this.stopAllNotes();
         this.linePaths.translate([0,(0-this.vertOffset)]);
         this.linePaths.removeChildren();
@@ -108,10 +110,24 @@ function PolarSynth(p) {
         this.lastNP = 0;
         this.lastVert = 0;
         this.vertOffset = 0;
-        $.getJSON( fileString , function(data) {self.startMusic(data)});
+        paper.project.importSVG(fileString+".svg",{insert:false,onLoad:this.setupSVG});
+        $.getJSON( fileString+".json" , function(data) {self.startMusic(data)});
     }
 
-    this.drawNote = function(nP,nT,nV) {
+    this.setupSVG = function(svgItem,svgStr) {
+        console.log(svgItem);
+        console.log(self);
+        var leftPosition = self.params.whichSide == "left" ? (canvasWidth * 0.1 + pngWidth / 2) : canvasWidth - (pngWidth / 2 + canvasWidth * 0.1);
+        console.log(leftPosition);
+        self.mapSVG = svgItem;
+        self.mapPaths.addChild(self.mapSVG);
+        self.mapPaths.setPosition(leftPosition, 350);
+        //self.mapPaths.fillColor = new paper.Color(1, 0, 0);
+        self.mapSVG.opacity = 0.3;
+    }
+
+    this.drawNote = function(nP,nT,nV,nRx,nRy) {
+        console.log(nRx+"\t"+nRy);
         var vertPosition = (nT / 50);
         var newPath = new paper.Path();
         newPath.strokeWidth = 2;
@@ -135,15 +151,17 @@ function PolarSynth(p) {
             noteTime = data.orderedPoints[i].time;
             noteDuration = data.orderedPoints[i].duration;
             noteVelocity = data.orderedPoints[i].velocity;
-            self.noteQueue.push(setTimeout(function(nP,nT,nV){
+            noteRawX = data.orderedPoints[i].rawx;
+            noteRawY = data.orderedPoints[i].rawy;
+            self.noteQueue.push(setTimeout(function(nP,nT,nV,nRx,nRy){
                 if (self.params.internalOrExternal == "external") {
                     self.externalMidiOut.playNote(nP,self.externalMidiChannel,{duration:noteDuration,velocity:noteVelocity});
                 } else {
                     MIDI.noteOn(self.internalMidiChannel, nP, noteVelocity, 0);
                     MIDI.noteOff(self.internalMidiChannel, nP,(noteDuration/1000));
                 }
-                self.drawNote(nP,nT,nV);
-            },noteTime,notePitch,noteTime,noteVelocity));
+                self.drawNote(nP,nT,nV,nRx,nRy);
+            },noteTime,notePitch,noteTime,noteVelocity,noteRawX,noteRawY));
         }
         if (isInstallation) setTimeout(self.startSynth,(noteTime+noteDuration));
     }
@@ -155,7 +173,7 @@ function PolarSynth(p) {
             var shouldBeZero = (self.lastVert + self.vertOffset) - ($(window).height() / 2);
             if ((shouldBeZero < -10 ) && (globalUpSpeed < -0.1)) {
                 globalUpSpeed = globalUpSpeed + 0.0005;
-            } else if ((shouldBeZero > 10)&& (globalUpSpeed > -0.4)) {
+            } else if ((shouldBeZero > 10) && (globalUpSpeed > -0.5)) {
                 globalUpSpeed = globalUpSpeed - 0.0005;
             }
         }
